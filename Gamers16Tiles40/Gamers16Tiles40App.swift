2b0e90ce91fc50bdb1ee71dedd20b7d698080003
Panteleev Exey <exeypanteleev@gmail.com>
2022-05-30 02:13:46 +0300

    Initial Commit

---
+++ Gamers16Tiles40/Gamers16Tiles40App.swift
@@ -0,0 +1,17 @@
+//
+//  Gamers16Tiles40App.swift
+//  Gamers16Tiles40
+//
+//  Created by exey on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Gamers16Tiles40App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
